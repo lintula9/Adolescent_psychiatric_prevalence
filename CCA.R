ac3870d593713d22c9611ae897cd903dca2d3f9c
lintula9 <lintula8@gmail.com>
2023-10-22 23:44:27 +0300

    CCA finishes. Excels could use some work now.

--- CCA.R
+++ CCA.R
@@ -1,7 +1,7 @@
 # Canonical Correlation Analysis CCA
 
 ccd <- scale(as.matrix( na.omit( df[ , c( PQBvars, BDIvars ) ] ) ))
-
+colnames(ccd) <- c(PQBvars, BDIvars)
 if (F){ # This takes long.
 
 ccaLambdas <- CCA::estim.regul( ccd[ , PQBvars ], # loocv and grid search for finding lambdas.
@@ -13,20 +13,28 @@ saveRDS( ccaLambdas, file = "CCAloocvLambdaSearch.R"  ) # file is retrievable by
 
 }
 if(F) {
-  readRDS("CCAloocvLambdaSearch.R") # Fetch ccaLambdas loocv result.
+  ccaLambdas <- readRDS("CCAloocvLambdaSearch.R") # Fetch ccaLambdas loocv result.
   }
 
-CCAResult <- CCA::rcc( ccd[ , PQBvars_cust ], 
+CCAResult <- CCA::rcc( ccd[ , PQBvars ], 
                        ccd[ , BDIvars ], 
                        lambda1 = ccaLambdas$lambda1 , lambda2 = ccaLambdas$lambda2 )
+if(F) {write.csv( x = do.call(cbind, list(CCAResult$xcoef[,1:3], 
+                                          CCAResult$scores$corr.X.xscores[,1:3], 
+                                          CCAResult$ycoef[,1:3],
+                                          CCAResult$scores$corr.Y.xscores[,1:3])) , 
+           file = "CCAresult.csv" )} # Save Result as csv.
+
 
 # Look if the most correlated structures in data are due to PQ-B 8, PQ-B 18?
-write.csv( x = cbind(CCAResult$xcoef[,1:3], CCAResult$ycoef[,1:3]) , 
-           file = "CCAresult.csv" )
 
-round(CCAResult$xcoef[,1:3], 2)              
+round(CCAResult$xcoef[,1:3], 2) # Regression coefficients.              
 round(CCAResult$ycoef[,1:3], 2)
-CCAResult$cor
+CCAResult$scores$corr.X.xscores # Correlations to respective canoncical component.
+CCAResult$scores$corr.Y.xscores
 
 
+
+
+plt.cc(CCAResult)
 
