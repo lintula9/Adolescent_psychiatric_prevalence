a229916dbd99302d644bba2c629325a89088e202
lintula9 <123648372+lintula9@users.noreply.github.com>
2024-04-16 21:36:53 +0300

    aa

--- Scripts/data_and_packages.R
+++ Scripts/data_and_packages.R
@@ -38,3 +38,13 @@ df$sex <- factor(df$Gender_Male1_Female2, levels = c(1,2), labels = c("Male", "F
 #missing pattern
 if(FALSE){md.pattern(df[,c(paste("PQB",1:21, sep = ""))])}
 
+write.csv(x = lavCor(df[ , c(BDIvars, PQBvars_cust)], ordered = T), file = "polycor.csv")
+write.csv(x = lavCor(df[ , c(BDIvars, PQBvars_cust)], ordered = T, output = "th"), file = "polycor_thresholds.csv")
+write.csv(x = cor(df[ , c(BDIvars, PQBvars_cust)], method = "spearman", use = "complete.obs"), file = "spearman.csv")
+subscales_and_sums = c(names(df)[grep("PQB[^0-9]",names(df))][c(11,1:5)],
+                       names(df)[grep("BDI",names(df))][43:46])
+write.csv(x = cor(df[ , subscales_and_sums ], use = "complete.obs"), 
+          file = "sums_and_subscales.csv")
+
+print(corr.test(df[ , c("PQBsum_cust", "BDI9")], use = "complete.obs"), short = F)
+
